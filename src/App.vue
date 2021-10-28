0d6e2df0a84e915dab372dbf36099cf25f1423ba
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2021-10-27 23:45:17 -0500

    Quitando el componente inicio de la App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import '@/sass/custom.sass'
-import Inicio from './components/Inicio.vue';
 import TheHeader from './components/layout/TheHeader.vue';
 </script>
 
